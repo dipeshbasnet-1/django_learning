0e4ff7864a9999484128b9aef04787a9624619fa
Dipesh Basnet <dipeshbasnet1db@gmail.com>
2026-06-16 20:26:25 +0545

    DAY UPDATES

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
-    path('login/', login, name='login')
-    
+    path('login/', login, name='login'),
 ]
