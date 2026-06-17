0571af56188e3e23a1528a267881c17ef2df9fe5
Dipesh Basnet <dipeshbasnet1db@gmail.com>
2026-06-17 20:47:19 +0545

    migrations, admin register

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Student, Department
+
 
 # Register your models here.
+
+# admin.site.register(Student)
+# admin.site.register(Department)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    search_fields=["name"]
+    ordering=["name"]
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=("id", "name", "email", "address", "age", "number", "department" )
+    
+    search_fields=["name", "email", "age"]
+    ordering=["name", "age"]
--- app/models.py
+++ app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+ModelField:
+    CharField()
+    TextField()
+    IntegerField()
+    FloatField()
+    DecimalField()
+    BooleanField()
+    DateField()
+    TimeField()
+    DateTimeField()
+    EmailField()
+    URLField()
+    ImageField()
+    FileField()
+    ForeignKey()
+
+Field_options:
+    null:True-> database can store null value
+    blank:True-> form field can be left empty while submitting form
+'''
+
+class Department(models.Model):
+    name=models.CharField(max_length=105)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    address=models.TextField(null=True, blank=True)
+    age=models.IntegerField()
+    number=models.CharField(max_length=15)
+    department=models.ForeignKey(Department, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.name}: {self.email}"
